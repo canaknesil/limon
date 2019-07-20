757930368e4365d7d7a3aab1e9a7ed77eae98ad4
Can Aknesil <aknesil@kth.se>
2019-07-20 15:19:17 +0200

    Reorganized directory structure.

--- scripts/run-win.sh
+++
@@ -1,4 +0,0 @@
-#! /bin/sh
-# This script is to run limon on windows subsystem for linux where gmp libraries are not automatically loaded.
-
-LD_LIBRARY_PATH=/usr/local/lib $(dirname "$0")/../bin/limon $@
